dd90de2a37cc20d487dc36284f436bc53432fb13
Vishal Yadav <vishalvishalyadav87@gmail.com>
2021-05-15 14:30:07 +0530

    added an authorization middleware for user

---
+++ middlewares/authorization.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next){
+  // get token from the header of the request
+  const token = req.header('x-auth-token');
+  if(!token)  return res.status(401).send('Access denied. No token provided.');
+  // it token provided, check for its validity
+  try {
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+    req.user = decoded;
+    next();
+  }
+  catch(ex){
+    res.status(400).send('Invalid token');
+  }
+}
