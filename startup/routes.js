908c26b8eed2fb7f644fbe7e4ec60bdca14e48b4
Vishal Yadav <vishalvishalyadav87@gmail.com>
2021-05-21 08:13:07 +0530

    added router for messages

--- startup/routes.js
+++ startup/routes.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const bodyparser = require('body-parser');
 const users = require('../routes/users');
 const conversations = require('../routes/conversations');
+const messages = require('../routes/messages');
 const authentication = require('../routes/authentication');
 
 // const error = require('../middlewares/error');
 
 module.exports = function(app) {
+  // app.use(cors());
   // app.use(express.json());
   // app.use(express.urlencoded({extended: true}));
   app.use(bodyparser.urlencoded({ extended: false }))
   app.use(bodyparser.json());
   app.use('/api/users', users);
   app.use('/api/conversations', conversations);
+  app.use('/api/conversations/:conversationId/messages', messages);
   app.use('/api/users/authentication', authentication);
   // app.use(error);
 }
