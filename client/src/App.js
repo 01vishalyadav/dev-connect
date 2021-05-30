b77a4d6043a10f3c501550016cf770eca7331f48
Vishal Yadav <vishalvishalyadav87@gmail.com>
2021-05-30 17:55:13 +0530

    now conversation list shows properly but with fewer details

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,96 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Home from './Home/Home';
 import SignInForm from './SignInForm/SignInForm';
 import SignUpForm from './SignUpForm/SignUpForm';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      isLoggedIn: false,
-      haveAccount: false,
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [haveAccount, setHaveAccount] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState();
+
+  const signUp = <SignUpForm signUpCompleted = {()=> signUpCompletedHandler()} haveAccountClicked={()=>haveAccountClickedHandler()} />
+  const signIn = <SignInForm signInCompleted = {()=> signInCompletedHandler() } dontHaveAccountClicked={()=>dontHaveAccountClickedHandler()} />
+  let form = signUp;
+
+  function checkIfAlreadyLoggedIn(){
+    if(!isLoggedIn){
+      setIsLoading(true);
+      const xAuthToken = localStorage.getItem('x-auth-token');
+      if(xAuthToken === null)  {
+        console.log('dont have token in localStorage');
+        setIsLoading(false);
+      }
+      else{
+        console.log('has token in localStorage');
+        let valid = false;
+        // present, but check if it is valid
+        axios.get('/api/users/me',{
+          headers: {
+            'x-auth-token': xAuthToken,
+          }
+        })
+        .then(res => {
+          console.log('has valid token', res);
+          setUser(res.data);
+          setIsLoggedIn(true);
+          setIsLoading(false);          
+        }).catch( (err)=>{
+          console.log("axios err:", err);
+          if(err.response){
+            console.log('axios received 4xx or 5xx');
+            console.log('rmoving token from localStorage');
+            localStorage.removeItem('x-auth-token');
+            setIsLoggedIn(false);
+            setIsLoading(false);   
+          }
+          else{
+            console.log('axios dont know what went wrong');
+            setIsLoggedIn(false);
+            setIsLoading(false);   
+          }
+        });
+      }
     }
   }
 
-
-  render(){
-    let form = null;
-    let dashboard = null;
-
-    let isLoggedIn = this.state.isLoggedIn;
-    let haveAccount = this.state.haveAccount;
-    if(isLoggedIn===false && haveAccount===true){
-      form = <SignInForm
-                dontHaveAccountClick={(e)=>this.setState({haveAccount:false})}
-                signInComplete={()=>this.setState({isLoggedIn:true})}
-             />
+  useEffect(()=>{
+    if(!isLoggedIn){
+      checkIfAlreadyLoggedIn();
+      if(haveAccount){
+        form = signIn;
+      }
     }
-    else if (isLoggedIn === false && haveAccount === false){
-      form = <SignUpForm 
-                signUpComplete={()=>this.setState({haveAccount:true})} haveAccountClick={(e)=>this.setState({haveAccount:true})} 
-            />
-    }
+  },[]);
 
-    else if(isLoggedIn){
-      dashboard = <div> <h3>Main Page</h3></div>
-    }
-    return (
-      <Container maxWidth="lg">
-        {form}
-        {dashboard}
-      </Container>
-    );
-  }  
+  function signUpCompletedHandler() {
+    checkIfAlreadyLoggedIn();
+  }
+  function signInCompletedHandler() {
+    checkIfAlreadyLoggedIn();
+  }
+  function haveAccountClickedHandler() {
+    setHaveAccount(true);
+  }
+  function dontHaveAccountClickedHandler(){
+    setHaveAccount(false);
+  }
+
+  function logout() {
+    localStorage.removeItem('x-auth-token');
+    setIsLoggedIn(false);
+  }
+
+  return (
+    <Container maxWidth="lg">
+      {
+        isLoading ? (<CircularProgress />)
+        :(isLoggedIn ? (<Home logout={()=>logout()} user={user} />):(haveAccount?signIn:signUp))
+      }
+    </Container>
+  );
 }
-
-export default App;
--- client/src/ConversationItemList/ConversationItemList.js
+++ client/src/ConversationItemList/ConversationItemList.js
@@ -1,16 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import App from '../App';
-import axios from 'axios';
+import getAllConversations from '../api/Conversations/getAllConversations';
 import ConversationItem from '../ConversationItem/ConversationItem';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import NotificationIcon from '@material-ui/icons/Notifications';
-import {red} from '@material-ui/core/colors';
 import Grid from '@material-ui/core/Grid';
 
 export default function ConversationItemList(props){
@@ -19,51 +11,33 @@ export default function ConversationItemList(props){
   const [conversationItems, setConversationItems] = useState();
   let userId=props.userId;
   let conversations = [];
+  console.log('ConversationItemList.js:- props.user:', props.user);
 
-  function conversationItemClickedHandler(conversationId){
+  function conversationItemClickedHandler(conversationId, conversationTitle, otherUser){
     // now hide conversationItemList and show messages for this conversation, to do so, return this signal to home, let it handle this
     // alert('conversationItem Clicked!');
     console.log('conItemList: got convId:', conversationId);
-    props.conversationItemClicked(conversationId);  
+    console.log('clicked on conversation having otherUser:', otherUser)
+    props.conversationItemClicked(conversationId, conversationTitle, otherUser);  
   }
 
   useEffect(()=>{
-    const xAuthToken = localStorage.getItem('x-auth-token');
-    if(xAuthToken === null)  {
-      console.log('dont have token in localStorage');
-      setIsLoading(false);
-    }
-    else{
-      console.log('has token in localStorage');
-      // present, but check if it is valid
-      axios.get('/api/conversations',{
-        headers: {
-          'x-auth-token': xAuthToken,
-        }
-      })
-      .then(res => {
-        conversations = res.data;
-        if(conversations.length>0){
-          let _conversationItems = conversations.map((conversation,index)=>{
-            return <ConversationItem conversation={conversation} key={conversation._id} userId={props.userId} conversationItemClicked={(conversationId)=>conversationItemClickedHandler(conversationId)} />
-          });
-          console.log('_conversationItemsInitialized');
-          setConversationItems(_conversationItems);
-        }
-        // console.log('users::::', users);
-        setIsLoading(false);          
-      }).catch( (err)=>{
-        console.log("axios err:", err);
-        if(err.response){
-          console.log('axios received 4xx or 5xx');
-          setIsLoading(false);   
-        }
-        else{
-          console.log('axios dont know what went wrong');
-          setIsLoading(false);   
-        }
-      });
-    }
+    getAllConversations().then((conversations)=>{
+      if(conversations.length>0){
+        let _conversationItems = conversations.map((conversation,index)=>{
+          return <ConversationItem 
+                  conversation={conversation} 
+                  key={conversation._id}
+                  user={props.user} 
+                  conversationItemClicked={(conversationId, conversationTitle, otherUser)=>conversationItemClickedHandler(conversationId, conversationTitle, otherUser)}
+
+                />
+        });
+        console.log('_conversationItemsInitialized');
+        setConversationItems(_conversationItems);
+      }
+    }).catch(err=>console.log("axios err:", err))
+      .finally(()=>setIsLoading(false));
   },[]);
 
   return(
