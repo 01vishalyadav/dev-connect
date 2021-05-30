c12f6b720f54535fba819ec11da6d29a4a5a2aba
Vishal Yadav <vishalvishalyadav87@gmail.com>
2021-05-30 18:07:30 +0530

    Now, current user will not be shown in FeedItemList

--- client/src/FeedItemList/FeedItemList.js
+++ client/src/FeedItemList/FeedItemList.js
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import App from '../App';
-import axios from 'axios';
 import getAllUsers from '../api/users/getAllUsers';
 import FeedItem from '../FeedItem/FeedItem';
 
@@ -10,17 +8,25 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
-export default function FeedItemList(){
+export default function FeedItemList(props){
   const [isLoading, setIsLoading] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [feedItems, setFeedItems] = useState();
-
+  function messageIconClickedHandler(otherUser){
+    props.feedItemMessageIconClicked(otherUser);
+  }
 
   useEffect(()=>{
     getAllUsers().then((users)=>{
       if(users.length>0){
-        let _feedItems = users.map((user,index)=>{
-          return <FeedItem user={user} key={user._id} />
+        // exclude the current user using filter
+        let _feedItems = users
+          .filter(user=> user._id !== props.user._id)
+          .map((user,index)=>{
+          return <FeedItem 
+                  user={user} 
+                  key={user._id} 
+                  messageIconClicked={(otherUser)=> messageIconClickedHandler(otherUser)} />
         });
         console.log('feedItemsInitialized');
         setFeedItems(_feedItems);
